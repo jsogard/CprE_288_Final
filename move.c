90ffc5e8ac89f4aa6e86c175bab7ff08354f68f9
Amupitan <amupitan@iastate.edu>
2017-04-19 19:06:55 +0100

    Added command look ups

---
+++ move.c
@@ -0,0 +1,48 @@
+#include "move.h"
+
+void move(oi_t *sensor, int centimeters){
+	if(centimeters == 0) return;
+
+	int dist = 0;
+	if(centimeters < 0)
+		oi_setWheels(-300,-300);
+	else
+		oi_setWheels(300,300);
+
+	while((centimeters < 0 && dist > centimeters) || (centimeters > 0 && dist < centimeters))
+	{
+		oi_update(sensor);
+		dist += sensor->distance;
+	}
+
+	oi_setWheels(0,0);
+}
+
+void turn(oi_t *sensor, int degrees){
+	if(degrees == 0) return;
+
+	int angle = 0;
+	if(degrees > 0)
+		oi_setWheels(-200,200);
+	else
+		oi_setWheels(200,-200);
+
+	while((degrees > 0 && angle > -degrees) || (degrees < 0 && angle < -degrees))
+	{
+		oi_update(sensor);
+		angle += sensor->angle;
+	}
+
+	oi_setWheels(0,0);
+}
+
+void escape(oi_t *sensor)
+{
+	move(sensor,-15);
+
+	turn(sensor,-90);
+	move(sensor,25);
+
+	turn(sensor,-90);
+	move(sensor,15);
+}
---
+++ move.h
@@ -0,0 +1,7 @@
+#include "open_interface.h"
+
+
+void move(oi_t *sensor, int centimeters);
+
+void turn(oi_t *sensor, int degrees);
+void escape(oi_t *sensor);
