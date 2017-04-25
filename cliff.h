2293c233bb8541ca428ec6322313362fef48b384
Wieczorek <ethantw@iastate.edu>
2017-04-24 21:35:59 -0500

    added header files

---
+++ cliff.h
@@ -0,0 +1,14 @@
+/*
+ * cliff.h
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: ethantw
+ */
+
+#ifndef CLIFF_H_
+#define CLIFF_H_
+
+int checkCliffs(oi_t *sensorData);
+
+
+#endif /* CLIFF_H_ */
---
+++ color.h
@@ -0,0 +1,14 @@
+/*
+ * color.h
+ *
+ *  Created on: Apr 24, 2017
+ *      Author: ethantw
+ */
+
+#ifndef COLOR_H_
+#define COLOR_H_
+
+int checkColors(oi_t *sensorData);
+
+
+#endif /* COLOR_H_ */
