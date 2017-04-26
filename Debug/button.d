468273b6d8c845cbe6c342169ebdade1120bb9b9
Ethan Wieczorek <ethantw@iastate.edu>
2017-04-26 01:59:56 -0500

    removed useless files

--- Debug/button.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-button.obj: ../button.c
-button.obj: ../button.h
-button.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
-
-../button.c: 
-../button.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
--- Debug/timer.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-timer.obj: ../timer.c
-timer.obj: ../Timer.h
-timer.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h
-
-../timer.c: 
-../Timer.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_15.12.1.LTS/include/stdint.h: 
